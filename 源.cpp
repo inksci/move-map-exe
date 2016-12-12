10c725cc928a32dbdc6b1f1b2f5059283b2efc01
inksci <inksci@outlook.com>
2016-12-12 18:06:29 +0800

    Done! Move using key, set .bmp as map

--- 源.cpp
+++ 源.cpp
@@ -1,271 +1,240 @@
+#include <windows.h>
+#include <iostream>
+#include <math.h>
+#include <GLU.h>
 #include <glut.h>
 #include <gl.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include <fstream>
+static GLint	imagewidth;
+static GLint	imageheight;
+static GLint	pixellength;
+static GLubyte	* pixeldata;
+
+/* use a named constant to approximate pi */
+const float pi = 3.14159265358979;
 
 
-float angle = 0.0;
-float red=1.0, blue=1.0, green=1.0;
-
-float x_move=0.0;
-float y_move=0.0;
-using namespace std;	ofstream outfile;
-
-// mouse click
-int mouse_left_down=0;
-int mouse_left_clicked=0;
-int mouse_right_down=0;
-int mouse_right_clicked=0;
-int mouse_x;
-int mouse_y;
-
-float robot_x;
-float robot_y;
+float	red	= 1.0, blue = 1.0, green = 1.0;
 
 
-void changeSize(int w, int h) 
+float				x_move	= 20.0;
+float				y_move	= 20.0;
+using namespace std; ofstream	outfile;
+
+/* mouse click */
+int	mouse_left_down		= 0;
+int	mouse_left_clicked	= 0;
+int	mouse_right_down	= 0;
+int	mouse_right_clicked	= 0;
+int	mouse_x;
+int	mouse_y;
+void MouseClick( int button, int state, int x, int y )
 {
-
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
-	if(h == 0)
-		h = 1;
-
-	float ratio = 1.0* w / h;
-
-	// Reset the coordinate system before modifying
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	// Set the viewport to be the entire window
-	glViewport(0, 0, w, h);
-
-	// Set the correct perspective.
-	gluPerspective(45,ratio,1,1000);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0,0.0,5.0, 
-		0.0,0.0,-1.0,
-		0.0f,1.0f,0.0f);
-
-
+	outfile << button << ", " << state << ", " << x << ", " << y << endl;
+	switch ( button )
+	{
+	case GLUT_LEFT_BUTTON:
+		if ( state == GLUT_DOWN )
+		{
+			mouse_left_down = 1;
+			mouse_x		= x;
+			mouse_y		= y;
+			;
+		}
+		break;
+	case GLUT_MIDDLE_BUTTON:
+	case GLUT_RIGHT_BUTTON:
+		if ( state == GLUT_DOWN )
+		{
+			mouse_right_down	= 1;
+			mouse_x			= x;
+			mouse_y			= y;
+			;
+		}
+		break;
+	default:
+		break;
+	}
 }
 
 
-void renderScene(void) 
+void processNormalKeys( unsigned char key, int x, int y )
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glPushMatrix();
-
-	float width=0.1;
-	float height=0.1;
-	glLoadIdentity();
-glTranslatef(x_move,y_move,-6.0f);
-	glBegin(GL_QUADS);				//绘制正方形
-	glVertex3f(-1.0f,1.0f,0.0f);	//四边形左上
-
-	glVertex3f(-1.0f+width,1.0f,0.0f);		//右上
-
-	glVertex3f(-1.0f+width,1.0+height,0.0f);	//右下
-
-	glVertex3f(-1.0f,1.0+height,0.0f);	//左下
-glEnd();
-
-
-// mouse event
-if(mouse_left_down==1){
-	if(mouse_left_clicked==0){
-		mouse_left_clicked=1;
-
-		// set value
-		robot_x=1;
-		robot_y=1;
-
-glLoadIdentity();
-glTranslatef(0.0f,0.0f,-6.0f);
-glBegin(GL_TRIANGLES);
-glVertex3f(0.0f,0.0f, 0.0f);
-glVertex3f(1.0f,0.0f, 0.0f);
-glVertex3f(0.0f,1.0f, 0.0f);
-glEnd();
-	}else{
-		// update value
-		//robot_x
-		glLoadIdentity();
-glTranslatef(0.0f,0.0f,-6.0f);
-glBegin(GL_TRIANGLES);
-glVertex3f(0.0f,0.0f, 0.0f);
-glVertex3f(1.0f,0.0f, 0.0f);
-glVertex3f(0.0f,1.0f, 0.0f);
-glEnd();
+	if ( key == 27 ) /*按ESC退出 */
+	{
+		outfile.close();
+		exit( 0 );
 	}
 
-	
+	switch ( key )
+	{
+	case 'w':
+		;
+		break;
+
+	default:
+		;
+		break;
+	}
+}
+
+
+void processSpecialKeys( int key, int x, int y )
+{
+	switch ( key )
+	{
+	case GLUT_KEY_F1: red	= 1.0; green = 0.0; blue = 0.0; break;
+	case GLUT_KEY_F2: red	= 0.0; green = 1.0; blue = 0.0; break;
+	case GLUT_KEY_F3: red	= 0.0; green = 0.0; blue = 1.0; break;
+
+	case GLUT_KEY_UP: y_move	= y_move + 1; break;
+	case GLUT_KEY_DOWN: y_move	= y_move - 1; break;
+	case GLUT_KEY_LEFT: x_move	= x_move - 1; break;
+	case GLUT_KEY_RIGHT: x_move	= x_move + 1; break;
+	}
+	outfile << "x_move: " << x_move << endl;
+	outfile << "y_move: " << y_move << endl;
 
 }
-	/*
-	glRotatef(angle,0.0,1.0,0.0);
-	glTranslatef(x_move,0.0f,-6.0f);
 
-	glColor3f(red,green,blue);
 
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-0.5,-0.5,0.0);
-	glVertex3f(0.5,0.0,0.0);
-	glVertex3f(0.0,0.5,0.0);
+void draw_rectangle( float l, float t, float w, float h )
+{
+	glBegin( GL_POLYGON );
+	/* 左下 左上 右上 右下 */
+	glVertex2f( l, t + h );
+	glVertex2f( l, t );
+	glVertex2f( l + w, t );
+	glVertex2f( l + w, t + h );
 	glEnd();
+}
 
 
-	glBegin(GL_QUADS);				//绘制正方形
-	glVertex3f(-1.0f,1.0f,0.0f);	//四边形左上
-
-	glVertex3f(1.0f,1.0f,0.0f);		//右上
-
-	glVertex3f(1.0f,-1.0f,0.0f);	//右下
-
-	glVertex3f(-1.0f,-1.0f,0.0f);	//左下
-	*/
+void draw_line()
+{
+	glVertex2f( 0, 0 );
+	glVertex2f( 640, 480 );
+}
 
 
-glLoadIdentity();
-glColor3f(1,green,blue);
-glTranslatef(0.0f,0.0f,-6.0f);
-glBegin(GL_TRIANGLES);
-glVertex3f(0.0f,0.0f, 0.0f);
-glVertex3f(1.0f,0.0f, 0.0f);
-glVertex3f(0.0f,1.0f, 0.0f);
-glEnd();
+void open_img()
+{
+	/* 打开文件 */
+	FILE* pfile = fopen( "C:\\Users\\Administrator\\Pictures\\map-1212.bmp", "rb" );
+	if ( pfile == 0 )
+		exit( 0 );
+	/* 读取图像大小 */
+	fseek( pfile, 0x0012, SEEK_SET );
+	fread( &imagewidth, sizeof(imagewidth), 1, pfile );
+	fread( &imageheight, sizeof(imageheight), 1, pfile );
+	/* 计算像素数据长度 */
+	pixellength = imagewidth * 3;
+	while ( pixellength % 4 != 0 )
+		pixellength++;
+	pixellength *= imageheight;
+	/* 读取像素数据 */
+	pixeldata = (GLubyte *) malloc( pixellength );
+	if ( pixeldata == 0 )
+		exit( 0 );
+	fseek( pfile, 54, SEEK_SET );
+	fread( pixeldata, pixellength, 1, pfile );
 
-glLoadIdentity();
-glTranslatef(0.2f,0.0f,-6.0f);
-glBegin(GL_TRIANGLES);
-glVertex3f(0.0f,0.0f, 0.0f);
-glVertex3f(1.0f,0.0f, 0.0f);
-glVertex3f(0.0f,1.0f, 0.0f);
-glEnd();
+	/* 关闭文件 */
+	fclose( pfile );
+}
 
-	glPopMatrix();
-	//angle++;
+
+/*
+ * --------------- setWindow ---------------------
+ * define our own function to specify the window, as explained later
+ */
+void setWindow( GLfloat left, GLfloat right, GLfloat bottom, GLfloat top )
+
+{
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	gluOrtho2D( left, right, bottom, top );
+}
+
+
+/* ---------------- setViewport ------------------ */
+void setViewport( int left, int right, int width, int height )
+/* define our own function to specify the viewport, as explained later */
+{
+	GLfloat bottom	= 0.5;
+	GLfloat top	= 1.5;
+	glViewport( left, bottom, right - left, top - bottom );
+}
+
+
+
+void myDisplay( void ) /* plot the sinc function, using world coordinates */
+
+{
+	glClear( GL_COLOR_BUFFER_BIT );
+	glDrawPixels( imagewidth, imageheight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixeldata );
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+
+
+	glColor3f( 0.0, 1.0, 0.0 );
+	draw_rectangle( x_move, y_move, 10, 10 );
+
+	glFlush();
 	glutSwapBuffers();
 }
 
-void MouseClick(int button, int state, int x, int y)  
-{ 
+
+void myInit( void )
+
+{
+	glClearColor( 1.0, 1.0, 1.0, 0.0 );
+	glColor3f( 0.0f, 0.0f, 0.0f );
+	glLineWidth( 1.0 );
+}
+
+
+void main( int argc, char** argv )
+
+{
+	outfile.open( "key-down.txt" );
+
+
+	open_img();
+	glutInit( &argc, argv );
+	glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );
+
+	int	m_winWidth	= 800;
+	int	m_winHeight	= 600;
+	glutInitWindowSize( m_winWidth, m_winHeight );
+	int	cx	= glutGet( GLUT_SCREEN_WIDTH );
+	int	cy	= glutGet( GLUT_SCREEN_HEIGHT );
+
+
 /*
-0, 0, 44, 51
-0, 1, 44, 51
-0, 0, 215, 62
-0, 1, 215, 62
-0, 0, 57, 150
-0, 1, 57, 150
-0, 0, 233, 171
-0, 1, 233, 171
-2, 0, 43, 42
-2, 1, 43, 42
-2, 0, 182, 45
-2, 1, 182, 45
-2, 0, 45, 153
-2, 1, 45, 153
-2, 0, 216, 165
-2, 1, 216, 165*/
-	outfile<<button<<", "<<state<<", "<<x<<", "<<y<<endl;
-   switch (button) {  
-      case GLUT_LEFT_BUTTON:  
-         if (state == GLUT_DOWN) { 
-			 mouse_left_down=1;
-			 mouse_x=x;
-				 mouse_y=y;
-            ;
-			
-		 }
-		 //glutIdleFunc(spinDisplay);  
-         break;  
-      case GLUT_MIDDLE_BUTTON:  
-      case GLUT_RIGHT_BUTTON:  
-         if (state == GLUT_DOWN) { 
-			 mouse_right_down=1;
-			 mouse_x=x;
-				 mouse_y=y;
-            ;
-		 }
-		 //glutIdleFunc(NULL);  
-         break;  
-      default:  
-         break;  
-   }  
-}  
-void processNormalKeys(unsigned char key, int x, int y) 
-{
-	
-	if (key == 27){  //按ESC退出
-		outfile.close();
-		exit(0);
-	}
+ * 假设窗口的宽度和高度分别是:m_winWidth, m_winHeight;
+ * 设置窗口居中：
+ */
+	glutInitWindowPosition( (cx - m_winWidth) / 2, (cy - m_winHeight) / 2 );
 
-	switch (key) {  
-      case 'w':
-		  angle++;
-		  break;
-		  
-	  default:
-		  ;
-		  break;
-	}
+	glutCreateWindow( "OpenGL-InkGrasp" );
+	glutDisplayFunc( myDisplay );
+	myInit();
+	/* call the function to specify the window */
+	setWindow( 0, 640, 0, 480 );
+	/* call the function to specify the viewport */
+	setViewport( 0, 640, 0, 480 );
+
+	glutIdleFunc( myDisplay ); /* 空闲时调用的函数，即渲染时调用的函数，非常重要 */
+	/* adding here the setting of keyboard processing */
+	glutKeyboardFunc( processNormalKeys );
+	glutSpecialFunc( processSpecialKeys );
+
+	glutMouseFunc( MouseClick );
+	glutMainLoop();
 }
 
-void processSpecialKeys(int key, int x, int y) 
-{
-	
 
-	switch(key) 
-	{
-	case GLUT_KEY_F1 : red = 1.0; green = 0.0; blue = 0.0; break;
-	case GLUT_KEY_F2 : red = 0.0; green = 1.0; blue = 0.0; break;
-	case GLUT_KEY_F3 : red = 0.0; green = 0.0; blue = 1.0; break;
-
-	case GLUT_KEY_UP : y_move=y_move+0.1; break;
-	case GLUT_KEY_DOWN : y_move=y_move-0.1; break;
-	case GLUT_KEY_LEFT : x_move=x_move-0.1; break;
-	case GLUT_KEY_RIGHT : x_move=x_move+0.1; break;
-	
-	}
-	outfile<<"x_move: "<<x_move<<endl;
-	outfile<<"y_move: "<<y_move<<endl;
-/*
-x_move: -1.5
-...
-x_move: 2.5
---------------
-y_move: -3.5
-...
-y_move: 0.5
-*/
-}
-
-void main(int argc, char **argv) 
-{
-	outfile.open("key-down.txt");
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(800,600);
-	glutCreateWindow("Lighthouse 3D - GLUT Tutorial");
-	glutDisplayFunc(renderScene);  //
-	/*
-	glutIdleFunc设置全局的回调函数，当没有窗口事件到达时，GLUT程序功能可以执行后台处理任务或连续动画。
-	如果启用，这个idle function会被不断调用，直到有窗口事件发生。
-	*/
-	glutIdleFunc(renderScene); //空闲时调用的函数，即渲染时调用的函数
-	glutReshapeFunc(changeSize);
-
-	//adding here the setting of keyboard processing
-	glutKeyboardFunc(processNormalKeys);
-	glutSpecialFunc(processSpecialKeys);
-
-	glutMouseFunc(MouseClick);  
-   //glutMotionFunc(MouseMotion);  
-
-	glutMainLoop(); //进入循环，该语句后面的语句不会执行
-}
